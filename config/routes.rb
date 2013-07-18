c9f14528ec4946b4a21f525bb23f701bc56c1a69
i5ting <i5ting@126.com>
2013-07-18 10:47:39 +0800

    add blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Bb::Application.routes.draw do
+  resources :posts
+
+
   get "welcome/index"
+  
+  match 'blog' => 'posts#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
